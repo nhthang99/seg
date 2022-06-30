5ba4ee742a32a8ddedce6afcb310c110dbc8021c
Thang Nguyen <nhthang99@outlook.com>
2022-06-30 17:18:52 +0700

    Update encoder wrapper

--- seg/encoders/__init__.py
+++ seg/encoders/__init__.py
@@ -1,11 +1,4 @@
-from seg.encoders.resnet import resnet_encoders
-from seg.encoders.vgg import vgg_encoders
-from seg.encoders.densenet import densenet_encoders
-from seg.encoders.mobilenet import mobilenet_encoders
+from seg.encoders.encoder import Encoder, cfg_encoders
 
 
-cfg_encoders = {}
-cfg_encoders.update(resnet_encoders)
-cfg_encoders.update(vgg_encoders)
-cfg_encoders.update(densenet_encoders)
-cfg_encoders.update(mobilenet_encoders)
+__all__ = ["Encoder", "cfg_encoders"]
--- seg/encoders/encoder.py
+++ seg/encoders/encoder.py
@@ -1,24 +1,50 @@
+from typing import Union
+
 import torch.nn as nn
 
-from seg.encoders import cfg_encoders
+from seg.encoders.resnet import resnet_encoders
+from seg.encoders.vgg import vgg_encoders
+from seg.encoders.densenet import densenet_encoders
+from seg.encoders.mobilenet import mobilenet_encoders
+
+
+cfg_encoders = {}
+cfg_encoders.update(resnet_encoders)
+cfg_encoders.update(vgg_encoders)
+cfg_encoders.update(densenet_encoders)
+cfg_encoders.update(mobilenet_encoders)
 
 
 class Encoder(nn.Module):
-    def __init__(self, backbone_name, pretrained=False, **kwargs) -> None:
+    def __init__(self, encoder: Union[str, nn.Module], pretrained=False, **kwargs) -> None:
         super(Encoder, self).__init__()
-        self.backbone_name = backbone_name
-        self.init_encoder(backbone_name, pretrained)
+        if isinstance(encoder, str):
+            self.encoder = self.get_encoder(encoder, pretrained)
+        else:
+            self.encoder = encoder
+        assert hasattr(self.encoder, "out_channels"), \
+               "Attribute `out_channels` does not defined in encoder"
 
-    def init_encoder(self, backbone_name, pretrained=False):
-        assert cfg_encoders[backbone_name].get("encoder") is not None, \
+    def get_encoder(self, encoder: str, pretrained=False) -> nn.Module:
+        """Get default encoder
+
+        Args:
+            encoder (str): see default supported encoders at encoders/ folder
+            pretrained (bool, optional): pretrained backbone. Defaults to False.
+
+        Returns:
+            nn.Module: encoder
+        """
+        assert cfg_encoders[encoder].get("encoder") is not None, \
                f"Encoder config should contain `encoder` attribute"
-        encoder_class = cfg_encoders[backbone_name]["encoder"]
-        params: dict = cfg_encoders[backbone_name].get("params", {})
-        params.update(dict(backbone_name=backbone_name, pretrained=pretrained))
-        self.encoder = encoder_class(**params)
+        encoder_class = cfg_encoders[encoder]["encoder"]
+        params: dict = cfg_encoders[encoder].get("params", {})
+        params.update(dict(backbone_name=encoder, pretrained=pretrained))
+        return encoder_class(**params)
 
+    @property
     def out_channels(self):
         return self.encoder.out_channels
 
     def forward(self, x):
-        return self.encoder(x)
+        return tuple(self.encoder(x).values())
