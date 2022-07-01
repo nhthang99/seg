bd1bdd28cce2e7eb3b6a9a9d30e98fcffea174ad
Thang Nguyen <nhthang99@outlook.com>
2022-07-01 15:56:36 +0700

    Add engine

---
+++ flame/engine.py
@@ -0,0 +1,29 @@
+from ignite import engine as e
+
+from flame.module import Module
+
+
+class Engine(Module):
+    '''
+        Base class for all engines. Your engine should subclass this class.
+        Class Engine contains an Ignite Engine that controls running process over a dataset.
+        Method _update is a function receiving the running Ignite Engine and the current batch in each iteration and returns data to be stored in the Ignite Engine's state.
+        Parameters:
+            dataset_name (str): dataset which engine run over.
+            device (str): device on which model and tensor is allocated.
+            max_epochs (int): number of epochs training process runs.
+    '''
+
+    def __init__(self, dataset, device, max_epochs=1):
+        super(Engine, self).__init__()
+        self.dataset = dataset
+        self.device = device
+        self.max_epochs = max_epochs
+        self.engine = e.Engine(self._update)
+
+    def run(self):
+        assert 'data' in self.frame, 'The frame does not have data.'
+        return self.engine.run(self.dataset, self.max_epochs)
+
+    def _update(self, engine, batch):
+        raise NotImplementedError
